027cff4f981b64f1a9c09d88448c33071ad1fa4f
crazyyy <crazyyy@gmail.com>
2016-07-18 16:12:37 +0300

    close project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
-(function () {
+(function() {
   var method;
-  var noop = function () {};
+  var noop = function() {};
   var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'];
   var length = methods.length;
   var console = (window.console = window.console || {});
@@ -11,9 +11,9 @@
 
     // Only stub undefined methods.
     if (!console[method]) {
-        console[method] = noop;
+      console[method] = noop;
     }
-}
+  }
 }());
 if (typeof jQuery == 'undefined') {
   console.warn('jQuery hasn\'t loaded');
@@ -22,3 +22,60 @@ if (typeof jQuery == 'undefined') {
 }
 // Place any jQuery/helper plugins in here.
 
+/** examples block - content with some height as images */
+$(document).ready(function() {
+  var examplesCarouselPhotosHeight = $('.examples-carousel__photos').height();
+  $('.examples-carousel__content').height(examplesCarouselPhotosHeight - 10);
+});
+/** examples block - gallery change photos */
+$(document).ready(function() {
+  $('.image-chooser li').on('click', function() {
+    if (!$(this).hasClass('currentimage')) {
+      var image = $(this).attr('dataimage');
+      $('.big-image img').attr('src', image);
+      $('.currentimage').removeClass('currentimage');
+      $(this).addClass('currentimage');
+    }
+  })
+});
+
+/** achievements__container some block sizes */
+
+$(document).ready(function() {
+  var maxheight = 0;
+  $(".achievements__container").each(function() {
+    if ($(this).height() > maxheight) {
+      maxheight = $(this).height();
+    }
+  });
+  $(".achievements__container").height(maxheight);
+  var maxheightNext = 0;
+  $(".achievements__sub_container").each(function() {
+    if ($(this).height() > maxheightNext) {
+      maxheightNext = $(this).height();
+    }
+  });
+  $(".achievements__sub_container").height(maxheightNext);
+
+});
+
+/** hidden-example */
+$(document).ready(function() {
+  $('.hidden-title').on('click', function(){
+    if ( $(this).hasClass('active') ) {
+      $(this).removeClass('active');
+      $('.hidden-example').fadeOut('fast');
+    } else {
+      $(this).addClass('active');
+      $('.hidden-example').fadeIn('fast');
+    }
+  })
+
+  $('.hidden-example__choose li').on('click', function() {
+    var image = $(this).attr('dataimage');
+    console.log(image)
+    $(this).parent().parent().css('background-image', 'url(' + image + ')');
+    $('.hidden-example__choose .active').removeClass('active');
+    $(this).addClass('active');
+  })
+});
